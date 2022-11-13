6e52a734960392f243020ed148bb66a04422d46b
shopnilashik <shopnil.ashik@gmail.com>
2022-11-13 17:24:11 +0600

    test

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -2,7 +2,7 @@ module.exports = (srv) => {
 
     // Reply mock data for Books...
     srv.on ('READ', 'Books', ()=>[
-      { ID:201, title:'Wuthering Heights', author_ID:101, stock:12 },
+      { ID:201, title:'Shopnil Ashik', author_ID:101, stock:12 },
       { ID:251, title:'The Raven', author_ID:150, stock:333 },
       { ID:252, title:'Eleonora', author_ID:150, stock:555 },
       { ID:271, title:'Catweazle', author_ID:170, stock:222 },
