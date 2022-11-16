39853e8dba7333519274c14ce0b24f09f852dc50
shopnilashik <shopnil.ashik@gmail.com>
2022-11-16 10:38:28 +0600

    show data in UI

--- app/cap/webapp/controller/View1.controller.js
+++ app/cap/webapp/controller/View1.controller.js
@@ -9,7 +9,36 @@ sap.ui.define([
 
         return Controller.extend("cap.cap.controller.View1", {
             onInit: function () {
+                //Get User details once logged in
 
+                //Get User details by using srv;
+
+                    // var y = '/sap/bc/ui2/start_up';
+
+                    // var xmlHttp = null;
+
+                    // xmlHttp = new XMLHttpRequest();
+
+                    // xmlHttp.onreadystatechange = function() {
+
+                    // if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+
+                    // var oUserData = JSON.parse(xmlHttp.responseText);
+
+                    //     console.log(oUserData);
+
+                    // }
+
+                    // };
+
+                    // xmlHttp.open( 'GET', y, false );
+
+                    // xmlHttp.send(null);
+
+
+                // oModel = new sap.ui.model.json.JSONModel();
+
+                // oModel.loadData('/sap/bc/ui2/start_up');
             }
         });
     });
